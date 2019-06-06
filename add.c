628bbae0648854108889e0856bef89fa96aa9f8b
Jamuna1998 <51438196+Jamuna1998@users.noreply.github.com>
2019-06-06 14:16:24 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,1 +1,21 @@
-
+#inclue<stdio.h>
+int main()
+{
+  int a,b,sum;
+  int getdata()
+  {
+    printf("enter the first number\n");
+    scanf("%d",&a);
+    printf("enter the second number\n");
+    scanf("%d",&b);
+  }
+  int add()
+  {
+    sum=a+b;
+    printf("sum is:"+sum);
+  }
+  int printdata()
+  {
+    printf("sum is:"+sum);
+  }
+}
