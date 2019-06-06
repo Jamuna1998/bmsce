8c65a7a8bce2953095a0a49503f858b3f99e2691
Jamuna1998 <51438196+Jamuna1998@users.noreply.github.com>
2019-06-06 14:21:24 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,21 +1,24 @@
 #inclue<stdio.h>
 int main()
 {
+  getdata();
+  add();
+  printdata();
+}
   int a,b,sum;
-  int getdata()
+  int getdata(int a,int b)
   {
     printf("enter the first number\n");
     scanf("%d",&a);
     printf("enter the second number\n");
     scanf("%d",&b);
   }
-  int add()
+  int add(int a,int b)
   {
     sum=a+b;
-    printf("sum is:%d",sum);
   }
-  int printdata()
+  int printdata(int sum)
   {
-    printf("sum is:"+sum);
+    printf("sum is:%d",sum);
   }
 }
