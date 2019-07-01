1e429ab6844dc67b9abb45dc3b5833f6972e72e5
Jamuna1998 <51438196+Jamuna1998@users.noreply.github.com>
2019-07-01 08:52:55 +0530

    Update addfraction.c

--- addfraction.c
+++ addfraction.c
@@ -1,36 +1,27 @@
 #include<stdio.h>
 
-struct fraction
+struct frac
 {
 	int num;
 	int deno;
 };
 
-void input_fraction(struct fraction f1,struct fraction f2)
-{	
-	printf("enter the first fraction\n");
+struct frac input(struct frac f1,struct frac f2)
+{	struct frac t;
+	printf("enter the fraction\n");
 	scanf("%d%d",&f1.num,&f1.deno);
-	printf("enter the second fraction\n");
-	scanf("%d%d",&f2.num,&f2.deno);	
+	printf("enter the fraction\n");
+	scanf("%d%d",&f1.num,&f1.deno);
+	f1=f2=t;
+	return t;
 }
 
-void sum_fraction(struct fraction f1,struct fraction f2,int *sum,int *total)
-{
-	
-	*sum=0;
-	*sum=(f1.num*f2.deno)+(f1.deno*f2.num);
-	*total=f1.deno*f2.deno;
-}
-void display_fraction(int sum,int total)
-{	
-	printf("sum=%d/%d",sum,total);
-}
-	
 int main()
 {	
-	struct fraction f1,f2;
-	int sum,total;
-	input_fraction(f1,f2);
-	sum_fraction(f1,f2,&sum,&total);
-	display_fraction(sum,total);
+	struct frac f1,f2;
+	input(f1,f2);
+	printf("%d%d",f1.num,f1.deno);
+	
 }
+
+	
